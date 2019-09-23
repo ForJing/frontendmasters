a202c81a2dccd84948e67e612135ebdbdf6176d1
forjing <xiaodoubao1994@gmail.com>
2019-09-23 20:31:56 +0800

    uodate

---
+++ digging-into-nodejs/ex1.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+"use strict";
+
+printHelp();
+
+// *******************
+
+function printHelp() {
+  console.log("ex1 usage:");
+  console.log("   ex1.js --help");
+  console.log("");
+  console.log("--help       print this help");
+  console.log("");
+}
